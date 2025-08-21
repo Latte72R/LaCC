75f0627fe5f1ff70851b782e7aa37d63e9085d94
Latte72 <ryofuji2005@gmail.com>
2025-08-21 14:27:56 +0900

    Update

--- src/parser/parse.c
+++ src/parser/parse.c
@@ -79,12 +79,16 @@ void error_duplicate_name(Token *tok, const char *type) {
 }
 
 void *safe_realloc_array(void *ptr, int elem_size, int need, int *cap) {
-  if (need > *cap) {
+  int old_cap = *cap;
+  *cap = (*cap > 0) ? *cap : 8;
+  while (need > *cap) {
     *cap = *cap * 2;
+  }
+  if (old_cap != *cap) {
     void *new_ptr = realloc(ptr, elem_size * *cap);
     if (!new_ptr)
       error("realloc failed");
-    return new_ptr;
+    ptr = new_ptr;
   }
   return ptr;
 }
