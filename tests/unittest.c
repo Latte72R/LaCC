dbe24f8e6aa029d33a02371cf1d89b90e6fe8c78
Latte72R <ryofuji2005@gmail.com>
2025-11-06 11:41:29 +0900

    Delete Mac OS compatibility tests

--- tests/unittest.c
+++ tests/unittest.c
@@ -23,7 +23,6 @@ void check_with_name(int result, const char *name, int ans) {
 #include "unittests/funcptrs_ternary_sizeof.c"
 #include "unittests/literals_and_switch.c"
 #include "unittests/loops_strings_arrays.c"
-#include "unittests/macos_compat.c"
 #include "unittests/macro.c"
 #include "unittests/nullable_funcptrs.c"
 #include "unittests/struct_arrays_bits.c"
@@ -199,7 +198,6 @@ int main() {
   CHECK(asm_compat_test2(), 10);
   CHECK(asm_compat_test3(), 21);
   CHECK(asm_compat_test4(), 40);
-  CHECK(mac_bitfield_test1(), 2);
 
   CHECK(comma_basic(), 2);
   CHECK(comma_assignments(), 10);
